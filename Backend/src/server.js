54caaf70af341f7e60194763f9dc171f248956f9
Mahesh Badgujar <badgujar282@gmail.com>
2025-11-06 15:16:20 +0530

    DataBase Issue Solved

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -2,7 +2,8 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-import app from "./app.js";
+import app from "./App.js"
+
 import { connectDB } from "./config/db.js";
 
 const PORT = process.env.PORT;

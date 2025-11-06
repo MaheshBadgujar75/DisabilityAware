e149de3261d13ea93a3185bbe6057b20349fda9f
Mahesh Badgujar <badgujar282@gmail.com>
2025-11-06 14:49:50 +0530

    Issue Solving

--- Backend/src/config/db.js
+++ Backend/src/config/db.js
@@ -9,10 +9,10 @@ export const connectDB = async () => {
   if (pool) return pool;
 
   pool = mysql.createPool({
-    host: process.env.DB_HOST || "127.0.0.1",
-    user: process.env.DB_USER || "root",
-    password: process.env.DB_PASS || "",
-    database: process.env.DB_NAME || "disabilitydb",
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
     waitForConnections: true,
     connectionLimit: Number(process.env.DB_CONN_LIMIT) || 10,
     queueLimit: 0,
